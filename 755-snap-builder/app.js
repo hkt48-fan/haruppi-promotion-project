162a7a5f4573d6ffc3f862955162c8fd1d5be49a
larvata <alzzlnewtype@gmail.com>
2015-08-20 16:25:29 +0800

    pretty posts.json

--- 755-snap-builder/app.js
+++ 755-snap-builder/app.js
@@ -20,17 +20,17 @@ request(metaUrl,function(err,res){
 
   var postUrl = "http://7gogo.jp/api/talk/post/list?direction=PREV&limit=30&postId=" + lastPostId + "&talkId=XDXHrpvEVMS9GtN76wEuUm%3D%3D";
 
-  console.log("ok")
+  console.log("ok");
 
-  console.log(postUrl)
+  console.log(postUrl);
 
   request(postUrl,function(err,res){
-    console.log("err?")
+    console.log("err?");
     if (err) {
       throw err;
     }
 
-    console.log("try")
+    console.log("try");
 
     var postData = JSON.parse(res.body);
     var posts = postData.posts;
@@ -38,25 +38,56 @@ request(metaUrl,function(err,res){
 
     var endOfYesterday = moment(0,"HH").add(-1,'s').valueOf()/1000;
     console.log("last");
-    console.log(endOfYesterday)
+    console.log(endOfYesterday);
 
 
     // get todays post
     var filtered = _.filter(posts,function(p){
+
+      delete p.talkId;
+      delete p.good;
+      delete p.sender;
+      delete p.sendUserImage;
+      delete p.userOfficialStatus;
+      delete p.rtCount;
+      delete p.shareUrl;
+      delete p.time;
+      delete p['delete'];
+
       // console.log(p)
       if (p.time> endOfYesterday) {
         _.each(p.body,function(b){
-          b.translate='';
+
+          if (b.bodyType === 4) {
+            var c= b.comment;
+            delete c.talkId;
+            delete c.commentId;
+            delete c.sender;
+            delete c.sendUserImage;
+            delete c.userOfficialStatus;
+            delete c.time;
+            delete c['delete'];
+
+            c.translate = '';
+            return;
+          }
+          else if (b.bodyType === 2 ) {
+            b.translate = b.image;
+          }
+          else{
+            b.translate = '';
+          }
+          
         });
 
         return true;
       }
     });
 
-    filtered = {posts:filtered}
+    filtered = {posts:filtered};
 
     console.log(posts.length);
-    console.log(filtered.length);
+    console.log(filtered.posts.length);
     fs.writeFileSync('posts.json',JSON.stringify(filtered,null,2));
 
   });
