ddae372fff1a90e85c95d63f20ef0a2e21213b6d
larvata <alzzlnewtype@gmail.com>
2015-11-06 01:32:43 +0800

    remove debug code

--- wechat-autorespond/middleware/luckyMoney.js
+++ wechat-autorespond/middleware/luckyMoney.js
@@ -5,7 +5,6 @@ module.exports = function(req,res,next){
     res.end('<!DOCTYPE html><html lang="en"><body><div>' + luckyMoneyStore.token + '<div><form method="POST" action="luckymoney"><input type="text" name="token" value="'+luckyMoneyStore.token+'"><input type="submit" value="update"></form></body></html>');
   }
   else if(req.method === 'POST') {
-    console.log(req.body)
     luckyMoneyStore.token = req.body.token;
     res.end('<!DOCTYPE html><html lang="en"><body><div>' + luckyMoneyStore.token + '<div><form method="POST" action="luckymoney"><input type="text" name="token" value="'+luckyMoneyStore.token+'"><input type="submit" value="update"></form></body></html>');
   }
