4c4443a482bbb71b5f736c3dd163c89cd1ebb866
larvata <alzzlnewtype@gmail.com>
2015-11-07 12:43:34 +0800

    fix typo

--- wechat-autorespond/middleware/replier.js
+++ wechat-autorespond/middleware/replier.js
@@ -149,7 +149,7 @@ var userMsgCommands = [
       content += '▪️perf/公演: 当日HKT48公演信息\n';
       content += '▪️hkt/开踢: HKT48冷知识\n';
       content += '▪️haruppi/儿玉遥: 兒玉遥迷你档案\n';
-      content += '▪️红包: 当天红包密令(双11限定,淘宝手机版中使用)\n';
+      content += '▪️红包: 当天红包密令(双11限定)\n';
       content += '▪️有任何建议请在这里留言';
 
       respd.xml.Content = content;
