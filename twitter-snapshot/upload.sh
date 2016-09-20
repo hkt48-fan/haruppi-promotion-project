0d30b55d71ea73ad10e9ad2940c38f726f20e670
Larvata <alzzlnewtype@gmail.com>
2016-09-20 16:26:24 +0800

    prevent move snapshot to upload folder when upload failed

--- twitter-snapshot/upload.sh
+++ twitter-snapshot/upload.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 scp `find ./snapshots -maxdepth 1 -type f -name '*_retina.png'` vultr.larvata.me:downloads/twitter
-mv snapshots/*_retina.png snapshots/uploaded
+if [[ $? != 0 ]]; then
+  echo 'Error occured.'
+else
+  mv snapshots/*_retina.png snapshots/uploaded
+fi
